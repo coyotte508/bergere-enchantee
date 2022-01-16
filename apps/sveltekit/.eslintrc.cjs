c67a595aeb0c0d563ed3803ed1296497d0eff138
coyotte508 <coyotte508@gmail.com>
2022-01-16 20:19:12 +0100

    ✨ Add unocss

--- apps/sveltekit/.eslintrc.cjs
+++ apps/sveltekit/.eslintrc.cjs
@@ -12,6 +12,10 @@ module.exports = {
 		sourceType: 'module',
 		ecmaVersion: 2020
 	},
+	rules: {
+		semi: ["error", "always"],
+		quotes: ["error", "double", { "avoidEscape": true }]
+	},
 	env: {
 		browser: true,
 		es2017: true,
--- apps/sveltekit/svelte.config.js
+++ apps/sveltekit/svelte.config.js
@@ -1,5 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from "@sveltejs/adapter-auto";
+import preprocess from "svelte-preprocess";
+import presetUno from "@unocss/preset-uno";
+import presetAttributify from "@unocss/preset-attributify";
+import UnoCss from "unocss/vite";
+import { extractorSvelte } from "@unocss/core";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +15,36 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: "#svelte", 
+
+		vite: {
+      plugins: [
+        UnoCss({
+          extractors: [extractorSvelte],
+					presets: [
+						presetUno(),
+						presetAttributify()
+					],
+					rules: [
+						[/^text-(.*)$/, ([, c], { theme }) => {
+							if (theme.colors[c])
+								return { color: theme.colors[c] };
+						}], 
+						[/^bg-(.*)$/, ([, c], { theme }) => {
+							if (theme.colors[c])
+								return { "background-color": theme.colors[c] };
+						}]
+					],
+          theme: {
+						colors: {
+							"oxford": "#0a274c",
+							"sunray": "#d3a95a",
+							"brunswick": "#1e4832"
+						},
+					}
+        })
+      ]
+    }
 	}
 };
 
