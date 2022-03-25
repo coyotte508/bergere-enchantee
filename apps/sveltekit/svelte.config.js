93f9379ccf00bc588dae725bfe055367eb6a9586
coyotte508 <coyotte508@gmail.com>
2022-03-25 14:23:51 +0100

    🚀 Switch to adapter-node

--- apps/sveltekit/svelte.config.js
+++ apps/sveltekit/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
 import preprocess from "svelte-preprocess";
 import presetUno from "@unocss/preset-uno";
 import presetAttributify from "@unocss/preset-attributify";
