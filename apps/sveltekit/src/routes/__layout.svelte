093ff129aa121ad7a5a1b7cc2ecb63e79809cca3
coyotte508 <coyotte508@gmail.com>
2022-03-26 00:38:01 +0100

    💬 Change link from pinterest to email

--- apps/sveltekit/src/routes/__layout.svelte
+++ apps/sveltekit/src/routes/__layout.svelte
@@ -152,8 +152,8 @@
       <a href="https://instagram.com/bergereenchantee" opacity-80 hover:opacity-100 style="width: 2rem; height: 2rem; padding: 0.35rem; padding-bottom: 0.25rem; padding-right: 0.3rem" bg-sunray rounded-full inline-block box-border mx-10>
         <div class="i-il-instagram" inline-block text-oxford w-full h-full>bergereenchantee</div>
       </a>
-      <a href="https://pinterest.com/bergereenchantee" opacity-80 hover:opacity-100 style="width: 2rem; height: 2rem; padding: 0.35rem; padding-bottom: 0.25rem; padding-right: 0.3rem" bg-sunray rounded-full inline-block box-border mx-10>
-        <div class="i-il-pin" inline-block text-oxford w-full h-full>bergereenchantee</div>
+      <a href="mailto:contact@bergereenchantee.fr" opacity-80 hover:opacity-100 style="width: 2rem; height: 2rem; padding: 0.35rem;  padding-right: 0.3rem" bg-sunray rounded-full inline-block box-border mx-10>
+        <div class="i-ant-design-mail-outlined" inline-block text-oxford w-full h-full></div>
       </a>
     </div>
     <div my-14>
