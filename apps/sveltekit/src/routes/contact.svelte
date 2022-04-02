0aca1e061f234ca8a0f781bf497a86a19e4295b0
Test User <coyotte508@gmail.com>
2022-04-02 18:35:48 +0200

    📱 Fix map width on contact page

--- apps/sveltekit/src/routes/contact.svelte
+++ apps/sveltekit/src/routes/contact.svelte
@@ -20,10 +20,10 @@
 
 <Container>
   <div text-center>
-    <iframe title="Carte" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21218.095703681618!2d-4.298313461852108!3d48.33626987923979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4816b59434b9b9a1%3A0x497027a2d3e4d4db!2s39%20Rte%20de%20l&#39;Argoat%2C%2029460%20Logonna-Daoulas!5e0!3m2!1sen!2sfr!4v1642367156653!5m2!1sen!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    <iframe w-full max-w-2xl height="450" title="Carte" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21218.095703681618!2d-4.298313461852108!3d48.33626987923979!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4816b59434b9b9a1%3A0x497027a2d3e4d4db!2s39%20Rte%20de%20l&#39;Argoat%2C%2029460%20Logonna-Daoulas!5e0!3m2!1sen!2sfr!4v1642367156653!5m2!1sen!2sfr" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
   </div>
   
-  <div max-w-xl mx-auto pt-3>
+  <div max-w-2xl mx-auto pt-3>
     {@html marked(pageData.text.description)}
   
     <ol pt-3>
