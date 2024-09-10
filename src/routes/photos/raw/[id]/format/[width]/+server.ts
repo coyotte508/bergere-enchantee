ef337179da48a7e289671fe92b201839b63687a8
coyotte508 <coyotte508@gmail.com>
2024-09-10 13:16:06 +0200

    ⚗️ Try caching images

--- src/routes/photos/raw/[id]/format/[width]/+server.ts
+++ src/routes/photos/raw/[id]/format/[width]/+server.ts
@@ -1,12 +1,13 @@
-import { error, redirect } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import type { RequestHandler } from './$types';
 import { collections } from '$lib/server/database';
-import { s3client, secureDownloadLink } from '$lib/server/s3';
+import { s3client } from '$lib/server/s3';
 import { GetObjectCommand } from '@aws-sdk/client-s3';
 import { S3_BUCKET } from '$env/static/private';
+import { sha256 } from '$lib/utils/sha256';
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async ({ params, request }) => {
 	const picture = await collections.pictures.findOne({
 		_id: params.id,
 		'storage.formats.width': +params.width,
@@ -22,17 +23,34 @@ export const GET: RequestHandler = async ({ params }) => {
 		throw error(500, "Impossible de trouver le format d'image demandé");
 	}
 
-	throw redirect(
-		302,
-		secureDownloadLink(
-			await getSignedUrl(
-				s3client,
-				new GetObjectCommand({
-					Bucket: S3_BUCKET,
-					Key: format.key,
-				}),
-				{ expiresIn: 24 * 3600 }
-			)
-		)
+	// Todo: store an etag directly when uploading the image
+	const etag = `"${await sha256(format.key)}"`;
+
+	console.log('etag', etag, request.headers.get('if-none-match'), format.key);
+	if (etag === request.headers.get('if-none-match')) {
+		return new Response(null, { status: 304 });
+	}
+
+	const url = await getSignedUrl(
+		s3client,
+		new GetObjectCommand({
+			Bucket: S3_BUCKET,
+			Key: format.key,
+			ResponseCacheControl: 'public, max-age=31536000',
+		})
 	);
+
+	const res = await fetch(url);
+
+	const headers = new Headers(res.headers);
+
+	if (res.ok) {
+		headers.set('Cache-Control', 'public, max-age=31536000');
+		headers.set('ETag', etag);
+	}
+
+	return new Response(res.body, {
+		status: res.status,
+		headers,
+	});
 };
