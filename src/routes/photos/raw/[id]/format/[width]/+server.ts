2505f2c3d278ae5f5e3a64a9fbffdf7db0598cad
coyotte508 <coyotte508@gmail.com>
2024-09-10 13:30:24 +0200

    🔥 Remove etag, caching header is enough

--- src/routes/photos/raw/[id]/format/[width]/+server.ts
+++ src/routes/photos/raw/[id]/format/[width]/+server.ts
@@ -5,9 +5,8 @@ import { collections } from '$lib/server/database';
 import { s3client, secureDownloadLink } from '$lib/server/s3';
 import { GetObjectCommand } from '@aws-sdk/client-s3';
 import { S3_BUCKET } from '$env/static/private';
-import { sha256 } from '$lib/utils/sha256';
 
-export const GET: RequestHandler = async ({ params, request }) => {
+export const GET: RequestHandler = async ({ params }) => {
 	const picture = await collections.pictures.findOne({
 		_id: params.id,
 		'storage.formats.width': +params.width,
@@ -23,27 +22,18 @@ export const GET: RequestHandler = async ({ params, request }) => {
 		throw error(500, "Impossible de trouver le format d'image demandé");
 	}
 
-	// Todo: store an etag directly when uploading the image
-	const etag = `"${await sha256(format.key)}"`;
-
-	console.log('etag', etag, request.headers.get('if-none-match'), format.key);
-	if (etag === request.headers.get('if-none-match')) {
-		return new Response(null, { status: 304 });
-	}
-
 	return new Response(null, {
 		status: 302,
 		headers: {
 			'Content-Type': 'image/jpeg',
-			'Cache-Control': 'public, max-age=31536000',
-			ETag: etag,
+			'Cache-Control': 'public, max-age=31536000, immutable',
 			location: secureDownloadLink(
 				await getSignedUrl(
 					s3client,
 					new GetObjectCommand({
 						Bucket: S3_BUCKET,
 						Key: format.key,
-						ResponseCacheControl: 'public, max-age=31536000',
+						ResponseCacheControl: 'public, max-age=31536000, immutable',
 					})
 				)
 			),
